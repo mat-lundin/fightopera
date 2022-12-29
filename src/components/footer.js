57f080c9f7267e5756e8c6f1950c563a714aec54
Mat Lundin <matlundin.dev@gmail.com>
2022-12-28 22:09:42 -0600

    init shows design

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,13 @@ export default function Footer() {
     return (
       <nav id='footer' className="navbar navbar-expand-lg navbar-light justify-content-center mt-2">
         <div className="m-2">
-          <a href="https://www.instagram.com/FightOpera/"><img src={process.env.PUBLIC_URL + "/images/instagram.svg"} alt="Instagram"></img></a>
+          <a href="https://www.instagram.com/FightOpera/"><img src={process.env.PUBLIC_URL + "/images/nav/instagram.svg"} alt="Instagram"></img></a>
         </div>
         <div className="m-2">
-          <a href="https://twitter.com/FightOpera"><img src={process.env.PUBLIC_URL + "/images/twitter.svg"} alt="Twitter"></img></a>
+          <a href="https://twitter.com/FightOpera"><img src={process.env.PUBLIC_URL + "/images/nav/twitter.svg"} alt="Twitter"></img></a>
         </div>
         <div className="m-2">
-          <a href="https://www.facebook.com/profile.php?id=100084837019977"><img src={process.env.PUBLIC_URL + "/images/facebook.png"} alt="Facebook"></img></a>
+          <a href="https://www.facebook.com/profile.php?id=100084837019977"><img src={process.env.PUBLIC_URL + "/images/nav/facebook.png"} alt="Facebook"></img></a>
         </div>
       </nav>
     )
--- src/components/shows.js
+++ src/components/shows.js
@@ -1,7 +1,13 @@
 const shows = ()=> {
     return(
         <div id='shows'>
+            <div className='banner'>
             <h1>Shows</h1>
+            </div>
+            <div className='event'>
+            <img src={process.env.PUBLIC_URL + "/images/IG/310159502_503923617884933_8729406778848083505_n.jpg"} alt='october show'></img>
+            <button>Tickets</button>
+            </div>
         </div>
     )
 };
