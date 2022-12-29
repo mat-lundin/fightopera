c0bbe7520b3629e02dc7b3d966607595e832f23f
Mat Lundin <matlundin.dev@gmail.com>
2022-12-29 16:44:35 -0600

    move tickets button and home banner text

--- src/components/shows.js
+++ src/components/shows.js
@@ -1,12 +1,15 @@
-const shows = ()=> {
-    return(
+const shows = () => {
+    return (
         <div id='shows'>
             <div className='banner'>
-            <h1>Shows</h1>
+                <h1>Shows</h1>
             </div>
             <div className='event'>
-            <img src={process.env.PUBLIC_URL + "/images/IG/310159502_503923617884933_8729406778848083505_n.jpg"} alt='october show'></img>
-            <button>Tickets</button>
+                <img src={process.env.PUBLIC_URL + "/images/IG/310159502_503923617884933_8729406778848083505_n.jpg"} alt='october show'></img>
+                <a href=''>
+                    <button>Tickets</button>
+                </a>
+
             </div>
         </div>
     )
