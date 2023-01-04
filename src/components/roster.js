5aea17725ebd5f639b26c0b6f7a669b1f9fa16b5
Mat Lundin <matlundin.dev@gmail.com>
2023-01-04 15:38:20 -0600

    add managers to roster

--- src/components/roster.js
+++ src/components/roster.js
@@ -6,6 +6,8 @@ const roster = () => {
             <div className="banner">
                 <h1>Roster</h1>
             </div>
+            <div className='wrestlers'>
+                <h2>Wrestlers</h2>
             <div className="wrestler">
                 <h3>Bones Mason</h3>
                 <Image src={process.env.PUBLIC_URL + '/images/IG/309057448_210397634663345_1524968459075524938_n.jpg'}></Image>
@@ -41,6 +43,46 @@ const roster = () => {
                         totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
                         quasi aliquam eligendi, placeat qui corporis!</p>
                 </div>
+            </div>
+            </div>
+            <div className='managers'>
+                <h2>Managers</h2>
+                <div className="wrestler">
+                <h3>Bones Mason</h3>
+                <Image src={process.env.PUBLIC_URL + '/images/IG/309057448_210397634663345_1524968459075524938_n.jpg'}></Image>
+                <div className='wrestlerBio'>
+                    <p>Perspiciatis
+                        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
+                        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+                        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
+                        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+                        doloremque. Quaerat provident commodi consectetur veniam similique ad
+                        earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
+                        fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
+                        suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+                        modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+                        totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
+                        quasi aliquam eligendi, placeat qui corporis!</p>
+                </div>
+            </div>
+            <div className="wrestler">
+                <h3>Smoke</h3>
+                <Image src={process.env.PUBLIC_URL + '/images/IG/310661502_1172373823625895_6729081499472376910_n.jpg'}></Image>
+                <div className='wrestlerBio'>
+                    <p>Perspiciatis
+                        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
+                        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+                        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
+                        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+                        doloremque. Quaerat provident commodi consectetur veniam similique ad
+                        earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
+                        fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
+                        suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+                        modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+                        totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
+                        quasi aliquam eligendi, placeat qui corporis!</p>
+                </div>
+            </div>
             </div>
         </div>
     )
