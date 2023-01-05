9c96c218659eefbdec7c88420c3ec904025c07f7
Mat Lundin <matlundin.dev@gmail.com>
2023-01-04 18:33:05 -0600

    init merch page

---
+++ src/components/merch.js
@@ -0,0 +1,36 @@
+const merch = () => {
+    return (
+        <div id='merch'>
+            <div className="banner">
+                <h1>Merch</h1>
+            </div>
+            <div className="aboutDesc">
+                <p>Perspiciatis
+                    minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
+                    quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+                    fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
+                    consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+                    doloremque. Quaerat provident commodi consectetur veniam similique ad
+                    earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
+                    fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
+                    suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+                    modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+                    totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
+                    quasi aliquam eligendi, placeat qui corporis!</p>
+                <p>Perspiciatis
+                    minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
+                    quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+                    fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
+                    consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+                    doloremque. Quaerat provident commodi consectetur veniam similique ad
+                    earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
+                    fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
+                    suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+                    modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+                    totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
+                    quasi aliquam eligendi, placeat qui corporis!</p>
+            </div>
+        </div>)
+}
+
+export default merch;
