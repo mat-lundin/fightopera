34ab6c47f63b850f3e5f9694510505d8001898a8
Mat Lundin <matlundin.dev@gmail.com>
2023-01-04 18:52:46 -0600

    add items, adjust banner img

--- src/components/merch.js
+++ src/components/merch.js
@@ -1,34 +1,33 @@
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 const merch = () => {
     return (
         <div id='merch'>
             <div className="banner">
                 <h1>Merch</h1>
             </div>
-            <div className="aboutDesc">
+            <div className="merchandise">
+                <div className="item">
+                    <h3>Item 1</h3>
+                    <Image src={process.env.PUBLIC_URL + '/images/IG/311906474_632504035076193_3940616914846406325_n.jpg'}></Image>
                 <p>Perspiciatis
                     minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
                     quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
                     fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
-                    consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-                    doloremque. Quaerat provident commodi consectetur veniam similique ad
-                    earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-                    fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
-                    suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-                    modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
-                    totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
-                    quasi aliquam eligendi, placeat qui corporis!</p>
+                    consequuntur!<br></br><br></br><Button>Buy</Button></p>
+                    
+                </div>
+                <div className="item">
+                    <h3>Item 2</h3>
+                    <Image src={process.env.PUBLIC_URL + '/images/IG/311906474_632504035076193_3940616914846406325_n.jpg'}></Image>
                 <p>Perspiciatis
                     minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
                     quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
                     fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
-                    consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-                    doloremque. Quaerat provident commodi consectetur veniam similique ad
-                    earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-                    fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
-                    suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-                    modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
-                    totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
-                    quasi aliquam eligendi, placeat qui corporis!</p>
+                    consequuntur!<br></br><br></br><Button>Buy</Button></p>
+                    
+                </div>
+
             </div>
         </div>)
 }
